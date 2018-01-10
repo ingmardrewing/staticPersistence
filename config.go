a99efc690c5f3bd5912c34f5fd8dbb219b958d51
Ingmar Drewing <ingmar@drewing.de>
2018-01-10 23:07:41 +0100

    Move config to staticPersistence

---
+++ config.go
@@ -0,0 +1,35 @@
+package staticPersistence
+
+import (
+	"strings"
+
+	"github.com/ingmardrewing/fs"
+)
+
+func NewConfig(path string) *Config {
+	c := new(Config)
+	c.ReadConfigFile(path)
+	return c
+}
+
+// Config reading from json data
+type Config struct {
+	Json
+	data []byte
+}
+
+func (c *Config) ReadDir(keys ...string) string {
+	path := c.Read(keys...)
+	if !strings.HasSuffix(path, "/") {
+		return path + "/"
+	}
+	return path
+}
+
+func (c *Config) Read(keys ...string) string {
+	return c.ReadString(c.data, keys...)
+}
+
+func (c *Config) ReadConfigFile(path string) {
+	c.data = fs.ReadByteArrayFromFile(path)
+}
