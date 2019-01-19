b9aa279a1c010b233eedd29202c47655a6e1ee8d
Ingmar Drewing <ingmar@drewing.de>
2019-01-19 16:52:32 +0100

    Add mass writing method

--- persistence.go
+++ persistence.go
@@ -1,8 +1,13 @@
 package staticPersistence
 
 import (
+	"fmt"
+	"strings"
+
 	"github.com/ingmardrewing/fs"
+	"github.com/ingmardrewing/rx"
 	"github.com/ingmardrewing/staticIntf"
+	log "github.com/sirupsen/logrus"
 )
 
 // dao versions
@@ -20,6 +25,32 @@ func ReadPagesFromDir(dir string) []staticIntf.PageDto {
 		dtos = append(dtos, getDto(fc))
 	}
 	return dtos
+}
+
+func WritePagesToDir(dtos []staticIntf.PageDto, dirname string) {
+	jsonName := "doc%05d.json"
+	counter := 0
+	for _, dto := range dtos {
+		newDto := fixcontent(dto)
+		fmt.Println(newDto.Content())
+		WritePageDtoToJson(newDto, dirname, fmt.Sprintf(jsonName, counter))
+		counter = counter + 1
+	}
+}
+
+func fixcontent(dto staticIntf.PageDto) staticIntf.PageDto {
+	cnew := strings.Replace(dto.Content(), `"`, `\"`, -1)
+	regex, err := rx.NewRx("\n|\r|\n\r")
+	if err != nil {
+		log.Error(err)
+	}
+	cnew = regex.SubstituteAllOccurences(cnew, "")
+	return NewFilledDto(dto.Id(),
+		dto.Title(), dto.TitlePlain(), dto.ThumbUrl(),
+		dto.ImageUrl(), dto.Description(), dto.DisqusId(),
+		dto.CreateDate(), cnew, dto.Url(), dto.Domain(),
+		dto.PathFromDocRoot(), dto.FsPath(), dto.HtmlFilename(),
+		dto.ThumbBase64(), dto.Category(), dto.MicroThumbUrl())
 }
 
 func getDto(fc fs.FileContainer) staticIntf.PageDto {
